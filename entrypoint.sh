df129a236a1f94bb5a5d0389eea68cbbefe2f2b3
woials <woials98@gmail.com>
2026-03-12 15:14:30 +0900

    [バグ修正・機能追加]　サーバを適切に起動できるように
    - pythonの仮想環境を有効化できないバグを修正
    - immich,jellyfin,flaskアプリそれぞれにtailscale funnel経由でアクセスできるように

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,18 +7,24 @@ set -e
 cd "$(dirname "$0")"
 
 echo "tailscaleを起動しています..."
-tailscale up
+sudo tailscale up
 
 # funnel_initializedファイルが存在しない場合
 if [ ! -f .funnel_initialized ]; then
     echo "tailscale funnelを開始しています..."
+    # 念のため既存の設定をクリア
+    tailscale serve reset
+    # 各サービスをtailscaleの内部ルーティングに登録
+    tailscale serve https:443 / http://localhost:5000
+    tailscale serve https:443 /photo http://localhost:2283
+    tailscale serve https:443 /video http://localhost:8096
     # tailscale funnelをバックグラウンドで起動
-    tailscale funnel --bg localhost:5000
+    tailscale funnel 443 on
     # touchコマンドでfunnel_initializedファイルを作成
     touch .funnel_initialized
 fi
 
-exec ./venv/bin/gunicorn -k gevent -w 4 \
+exec ./venv/bin/gunicorn -k gevent -w 2 \
     --timeout 300 \
     --keep-alive 10 \
     -b 0.0.0.0:5000 \
